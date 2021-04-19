874f8662c3b0554cc82eccd6b2377b2ed13c9f17
ZinovinIvan <79217390+ZinovinIvan@users.noreply.github.com>
2021-04-19 14:36:08 +0300

    1. [ex2-34] Установить значение тега meta «specialdate» в компоненте news.list

---
+++ local/templates/furniture_dark-blue/components/bitrix/news/.default/bitrix/news.list/.default/lang/ru/template.php
@@ -0,0 +1,4 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$MESS['MEWS_DETAIL_LINK'] = 'Читать дальше';
+$MESS ['NEWS_DELETE_CONFIRM'] = 'Вы уверены, что хотите удалить новость?';
+?>
