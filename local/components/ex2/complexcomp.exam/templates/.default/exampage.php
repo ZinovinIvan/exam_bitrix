39a68d941fb502221223e08d17af8266757292cf
ZinovinIvan <79217390+ZinovinIvan@users.noreply.github.com>
2021-05-04 15:13:05 +0300

    5. [ex2-25] Создание комплексного компонента «Моя фотогалерея»

---
+++ local/components/ex2/complexcomp.exam/templates/.default/exampage.php
@@ -0,0 +1,5 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+echo "PARAM1=".$arResult['VARIABLES']['PARAM1']."<br>";
+echo "PARAM2=".$arResult['VARIABLES']['PARAM2'];
+?>
